b9d9d1eca769333b9b1519d79eb00ed1132bb64a
sarawutb <sarawut.bcs59@ubru.ac.th>
2022-06-29 17:33:53 +0700

    up

--- manager/page1.php
+++ manager/page1.php
@@ -146,7 +146,7 @@ $data = json_decode($stmt);
       </tbody>
     </table>
     <?php if (!empty($data->data)) { ?>
-      <nav aria-label="Page navigation example">
+      <!-- <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-end">
           <li class="page-item">
             <a class="page-link" href="#">ย้อนกลับ</a>
@@ -161,7 +161,7 @@ $data = json_decode($stmt);
             <a class="page-link" href="#">ถัดไป</a>
           </li>
         </ul>
-      </nav>
+      </nav> -->
     <?php } ?>
   </div>
 </div>
@@ -283,7 +283,7 @@ $data = json_decode($stmt);
   $(function() {
     $('table#table').each(function() {
       var currentPage = 0;
-      var numPerPage = 2;
+      var numPerPage = 9;
       var $table = $(this);
       $table.bind('repaginate', function() {
         $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
@@ -291,11 +291,11 @@ $data = json_decode($stmt);
       $table.trigger('repaginate');
       var numRows = $table.find('tbody tr').length;
       var numPages = Math.ceil(numRows / numPerPage);
-      var $pager = $('<div class="pager"></div>');
+      var $pager = $('<ul class="pagination justify-content-end"></ul>');
       var $previous = $('<li class="page-item"><a class="page-link" href="#">ย้อนกลับ</a></li>');
-      var $next = $('<span class="next">>></spnan>');
+      var $next = $('<li class="page-item"><a class="page-link" href="#">ถัดไป</a></li>');
       for (var page = 0; page < numPages; page++) {
-        $('<span class="page-number"></span>').text(page + 1).bind('click', {
+        $('<a class="page-item page-link" href="#"></a>').text(page + 1).bind('click', {
           newPage: page
         }, function(event) {
           currentPage = event.data['newPage'];
@@ -303,27 +303,27 @@ $data = json_decode($stmt);
           $(this).addClass('active').siblings().removeClass('active');
         }).appendTo($pager).addClass('clickable');
       }
-      $pager.insertAfter($table).find('span.page-number:first').addClass('active');
-      $previous.insertBefore('span.page-number:first');
-      $next.insertAfter('span.page-number:last');
+      $pager.insertAfter($table).find('a.page-item:first').addClass('active');
+      $previous.insertBefore('a.page-item:first');
+      $next.insertAfter('a.page-item:last');
 
       $next.click(function(e) {
         $previous.addClass('clickable');
-        $pager.find('.active').next('.page-number.clickable').click();
+        $pager.find('.active').next('.page-item.clickable').click();
       });
       $previous.click(function(e) {
         $next.addClass('clickable');
-        $pager.find('.active').prev('.page-number.clickable').click();
+        $pager.find('.active').prev('.page-item.clickable').click();
       });
       $table.on('repaginate', function() {
         $next.addClass('clickable');
         $previous.addClass('clickable');
 
         setTimeout(function() {
-          var $active = $pager.find('.page-number.active');
-          if ($active.next('.page-number.clickable').length === 0) {
+          var $active = $pager.find('.page-item.active');
+          if ($active.next('.page-item.clickable').length === 0) {
             $next.removeClass('clickable');
-          } else if ($active.prev('.page-number.clickable').length === 0) {
+          } else if ($active.prev('.page-item.clickable').length === 0) {
             $previous.removeClass('clickable');
           }
         });
